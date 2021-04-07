e2ca6868684e7a0764353490a746a60c04797570
Juliana Balcero <s288025@studenti.polito.it>
2021-04-06 23:12:26 -0500

    Points 5,6,7

--- CarPooling/app/src/main/java/it/polito/mad/carpooling/EditProfileActivity.kt
+++ CarPooling/app/src/main/java/it/polito/mad/carpooling/EditProfileActivity.kt
@@ -1,27 +1,41 @@
 package it.polito.mad.carpooling
 
+
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.ContextMenu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
+
 
 class EditProfileActivity : AppCompatActivity() {
     // Declaration of properties
+
     private lateinit var editText: EditText
     private lateinit var editText2: EditText
     private lateinit var editText3: EditText
     private lateinit var editText4: EditText
+    private lateinit var buttonSave: Button
+    private lateinit var imageView : ImageView
+
+    private lateinit var sharedPreferences: SharedPreferences
+    //https://medium.com/swlh/sharedpreferences-in-android-using-kotlin-6d3bb4ffb71c
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_edit_profile)
 
         // iniziatilation of properties
@@ -29,10 +43,82 @@ class EditProfileActivity : AppCompatActivity() {
         editText2 =findViewById<EditText>(R.id.editText2)
         editText3 =findViewById<EditText>(R.id.editText3)
         editText4 =findViewById<EditText>(R.id.editText4)
+        buttonSave =findViewById<Button>(R.id.buttonSave)
+        imageView = findViewById<ImageView>(R.id.imageView)
+
+        sharedPreferences = getSharedPreferences("SHARED_PREF",Context.MODE_PRIVATE)
+        //Here we retrieve the final values of the variables on ShowProfileActivity
+        val full_name_init: String? = intent.getStringExtra("group22.lab1.FULL_NAME")
+        editText.setText(full_name_init)
+        val nick_name_init: String? = intent.getStringExtra("group22.lab1.Nickname")
+        editText2.setText(nick_name_init)
+        val email_add_init: String? = intent.getStringExtra("group22.lab1.email")
+        editText3.setText(email_add_init)
+        val user_loca_init: String? = intent.getStringExtra("group22.lab1.Location")
+        editText4.setText(user_loca_init)
+
+        //Here we saved the new information the user write
+        buttonSave.setOnClickListener {
+
+
+            // we save the values typed on the editText into strings
+            val full_name: String = editText.text.toString()
+            val nick_name: String = editText2.text.toString()
+            val email_add: String = editText3.text.toString()
+            val user_loca: String = editText4.text.toString()
+
+            // we save the values using sharedPreferences to retrieve later
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString("FULL_NAME",full_name)
+            editor.putString("NICK_NAME",nick_name)
+            editor.putString("EMAIL_ADD",email_add)
+            editor.putString("USER_LOCA",user_loca)
+            editor.apply()
+            Toast.makeText(this,"Data saved",Toast.LENGTH_SHORT).show()
+
+            //We save the info in the intent and return to ShowProfileActivity
+            val intent = Intent(this, ShowProfileActivity::class.java)
+            intent.putExtra("group22.lab1.FULL_NAME",full_name)
+            intent.putExtra("group22.lab1.Nickname",nick_name)
+            intent.putExtra("group22.lab1.email",email_add)
+            intent.putExtra("group22.lab1.Location",user_loca)
+            setResult(Activity.RESULT_OK,intent)
+            finish()
+        }
+
+
 
         val cameraButton = findViewById<ImageButton>(R.id.cameraButton)
         registerForContextMenu(cameraButton)
+
+        //here we launch the camera
+        val CAMERA_REQUEST_CODE = 0
+        cameraButton.setOnClickListener{              val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if(callCameraIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
+            }
+        }
+
+
     }
+
+    // Here we save the picture taken by the camera and update the picture on the profile user
+    val CAMERA_REQUEST_CODE = 0
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            CAMERA_REQUEST_CODE -> {
+                if(resultCode == Activity.RESULT_OK && data != null){
+                    imageView.setImageBitmap(data?.extras?.get("data") as Bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
     override fun onCreateContextMenu(menu: ContextMenu, v: View,
                                      menuInfo: ContextMenu.ContextMenuInfo?) {
@@ -46,9 +132,11 @@ class EditProfileActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.gallery -> {
                 true
+
             }
             R.id.use_camera -> {
                 true
+
             }
             else -> super.onContextItemSelected(item)
         }
@@ -63,6 +151,7 @@ class EditProfileActivity : AppCompatActivity() {
         outState.putString("ed2",editText2.text.toString())
         outState.putString("ed3",editText3.text.toString())
         outState.putString("ed4",editText4.text.toString())
+
     }
 
     // Now we restore the information
--- CarPooling/app/src/main/java/it/polito/mad/carpooling/ShowProfileActivity.kt
+++ CarPooling/app/src/main/java/it/polito/mad/carpooling/ShowProfileActivity.kt
@@ -1,16 +1,52 @@
 package it.polito.mad.carpooling
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
+import android.widget.EditText
 
 class ShowProfileActivity : AppCompatActivity() {
+    //private lateinit var textView: TextView
+    private lateinit var preferences: SharedPreferences
+    private lateinit var textView: TextView
+    private lateinit var textView2: TextView
+    private lateinit var textView3: TextView
+    private lateinit var textView4: TextView
+    val REQUEST_CODE = 222
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.show_profile)
+
+        // iniziatilation of properties
+        textView =findViewById<TextView>(R.id.textView)
+        textView2 =findViewById<TextView>(R.id.textView2)
+        textView3 =findViewById<TextView>(R.id.textView3)
+        textView4 =findViewById<TextView>(R.id.textView4)
+
+        preferences = getSharedPreferences("SHARED_PREF",Context.MODE_PRIVATE)
+        // Retrieve the information when the app has been closed with Shared Preferences
+        val full_name = preferences.getString("FULL_NAME","")
+        if (full_name!=""){textView.text = full_name} else{textView.setText("Full Name")}
+
+        val nick_name = preferences.getString("NICK_NAME","")
+        if (nick_name!=""){textView2.text = nick_name} else{textView2.setText("Nickname")}
+
+        val email_add = preferences.getString("EMAIL_ADD","")
+        if (email_add!=""){textView3.text = email_add} else{textView3.setText("email@address")}
+
+        val user_loca = preferences.getString("USER_LOCA","")
+        if (user_loca!=""){textView4.text = user_loca} else{textView4.setText("Location")}
+
+
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
@@ -20,8 +56,44 @@ class ShowProfileActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle item selection
         when (item.itemId) {
-
+            R.id.edit_profile -> {
+                true
+                editProfile()
+            }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    //We save the info in the intent and return to EditProfileActivity
+    private fun editProfile(){
+        // Inside this method, creat an explicit intent targeting
+        // the EditProfileActivity class
+        //CAMBIO DE PANTALLA, CUANDO PRESIONO EN SAVE
+        val full_name: String = textView.text.toString()
+        val nick_name: String = textView2.text.toString()
+        val email_add: String = textView3.text.toString()
+        val user_loca: String = textView4.text.toString()
+        val intent = Intent(this, EditProfileActivity::class.java)
+        intent.putExtra("group22.lab1.FULL_NAME",full_name)
+        intent.putExtra("group22.lab1.Nickname",nick_name)
+        intent.putExtra("group22.lab1.email",email_add)
+        intent.putExtra("group22.lab1.Location",user_loca)
+
+        startActivityForResult(intent,REQUEST_CODE)
+        //startActivity(intent)
+        //finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                textView.text=data?.getStringExtra("group22.lab1.FULL_NAME")
+                textView2.text=data?.getStringExtra("group22.lab1.Nickname")
+                textView3.text=data?.getStringExtra("group22.lab1.email")
+                textView4.text=data?.getStringExtra("group22.lab1.Location")
+
+            }
+        }
     }
 }
